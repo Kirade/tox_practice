e0353e1e2c3d293dd53c45dea28aee6028c52f2f
JaeyoungHeo <jay.jaeyoung@gmail.com>
2020-11-29 18:56:40 +0900

    Add gitignore sqlite

--- tox_practice/settings.py
+++ tox_practice/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'tox_practice.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR) + '/db.sqlite3',
+        'NAME': str(BASE_DIR) + '/' + 'db.sqlite3',
     }
 }
 
